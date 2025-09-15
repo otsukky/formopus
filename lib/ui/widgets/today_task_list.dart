39d8a23be54eea96c1807a44e6bb9d0600d3abe9
月岡陽人 <87177693+otsukky@users.noreply.github.com>
2025-09-15 12:45:40 +0900

    fix: モーダルを切り出し

--- lib/ui/widgets/today_task_list.dart
+++ lib/ui/widgets/today_task_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:formopus/domain/status.dart';
 import 'package:formopus/domain/task.dart';
+import 'package:formopus/ui/widgets/delete_confirmation.dart';
 
 class TodayTaskList extends StatefulWidget {
   const TodayTaskList({super.key, required this.tasks});
@@ -13,53 +14,17 @@ class TodayTaskList extends StatefulWidget {
 }
 
 class _TodayTaskListState extends State<TodayTaskList> {
-  void _deleteTask(Task taskToDelete) async {
+  void _deleteTask(Task task) async {
     final theme = Theme.of(context);
+    final taskTitle = task.title;
     final bool? shouldDelete = await showModalBottomSheet<bool>(
-      context: context, // 現在の BuildContext
+      context: context,
       backgroundColor: theme.colorScheme.surface,
       builder: (BuildContext bc) {
-        return Container(
-          padding: const EdgeInsets.all(16.0),
-          // コンテンツの高さに合わせてサイズ調整
-          child: Wrap(
-            children: <Widget>[
-              Center(
-                child: Text(
-                  '「${taskToDelete.title}」を本当に削除しますか？',
-                  style: theme.textTheme.titleMedium,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(height: 48),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FilledButton.tonal(
-                    style: FilledButton.styleFrom(
-                      backgroundColor:
-                          theme.colorScheme.surfaceContainerHighest,
-                      foregroundColor: theme.colorScheme.onSurface,
-                    ),
-                    child: const Text('キャンセル'),
-                    onPressed: () {
-                      Navigator.of(bc).pop(false);
-                    },
-                  ),
-                  FilledButton.tonal(
-                    style: FilledButton.styleFrom(
-                      backgroundColor: theme.colorScheme.errorContainer,
-                      foregroundColor: theme.colorScheme.onErrorContainer,
-                    ),
-                    child: const Text('削除'),
-                    onPressed: () {
-                      Navigator.of(bc).pop(true);
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+        return DeleteConfirmation(
+          taskTitle: taskTitle,
+          theme: theme,
+          bottomSheetContext: bc,
         );
       },
     );
@@ -67,12 +32,10 @@ class _TodayTaskListState extends State<TodayTaskList> {
     if (!mounted) return;
 
     if (shouldDelete == true) {
-      setState(() {
-        widget.tasks.remove(taskToDelete);
-      });
+      setState(() => widget.tasks.remove(task));
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text("${taskToDelete.title} を削除しました"),
+          content: Text("$taskTitle を削除しました"),
           duration: const Duration(seconds: 2),
         ),
       );
