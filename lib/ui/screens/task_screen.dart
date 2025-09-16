be611edca6b0f2e44ec12f6531b98ccfa5c74fc7
月岡陽人 <87177693+otsukky@users.noreply.github.com>
2025-09-17 00:00:13 +0900

    fix: タスクをデータレイヤーから呼び出す

--- lib/ui/screens/task_screen.dart
+++ lib/ui/screens/task_screen.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
 
-import '../../domain/status.dart';
-import '../../domain/task.dart';
+import '../viewmodel/task_view_model.dart';
 import '../widgets/create_task.dart';
 import '../widgets/daily_summary.dart';
 import '../widgets/formopus_app_bar.dart';
@@ -17,78 +17,61 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-  final List<Task> _tasks = [
-    Task(
-      title: "メールに返信",
-      memo: "Aさんからのお誘いに回答",
-      deadline: DateTime(2025, 1, 1),
-      status: Status.incomplete,
-    ),
-    Task(
-      title: "ジムに行く",
-      memo: "",
-      deadline: DateTime(2025, 1, 1),
-      status: Status.incomplete,
-    ),
-    Task(
-      title: "買い物",
-      memo: "ティッシュを買うの忘れずに",
-      deadline: DateTime(2025, 1, 1),
-      status: Status.incomplete,
-    ),
-    Task(
-      title: "写真の整理",
-      memo: "",
-      deadline: DateTime(2025, 1, 1),
-      status: Status.incomplete,
-    ),
-    Task(
-      title: "今週の開発",
-      memo: "コンポーネント1個作る",
-      deadline: DateTime(2025, 1, 1),
-      status: Status.incomplete,
-    ),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<TaskViewModel>(context, listen: false).loadTasks();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: FormopusAppBar(title: widget.title),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            // TODO 登録されたタスクに応じてサマリを表示する
-            DailySummary(date: DateTime(2025, 1, 1), total: 10, completed: 2),
-            Expanded(child: TodayTaskList(tasks: _tasks)),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _createTask,
-        tooltip: "タスクの追加",
-        shape: CircleBorder(),
-        child: const Icon(Icons.add),
-      ),
+    return Consumer<TaskViewModel>(
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          appBar: FormopusAppBar(title: widget.title),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // TODO 登録されたタスクに応じてサマリを表示する
+                DailySummary(
+                  date: DateTime(2025, 1, 1),
+                  total: viewModel.tasks.length,
+                  completed: viewModel.tasks
+                      .where((task) => task.status.isComplete())
+                      .length,
+                ),
+                Expanded(child: TodayTaskList(tasks: viewModel.tasks)),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _createTask(context),
+            tooltip: "タスクの追加",
+            shape: CircleBorder(),
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 
-  void _createTask() async {
+  void _createTask(BuildContext dialogContext) async {
     final newTaskDetails = await showDialog<Map<String, dynamic>>(
-      context: context,
-      builder: (BuildContext dialogContext) => CreateTaskDialog(),
+      context: dialogContext,
+      builder: (BuildContext innerDialogContext) => CreateTaskDialog(),
     );
 
     if (!mounted) return;
 
     if (newTaskDetails != null) {
-      final newTask = Task(
-        title: newTaskDetails["title"] as String,
-        memo: newTaskDetails["memo"] as String,
-        deadline: newTaskDetails["deadline"] as DateTime,
-        status: Status.incomplete,
+      await Provider.of<TaskViewModel>(context, listen: false).createTask(
+        newTaskDetails["title"] as String,
+        newTaskDetails["memo"] as String,
+        newTaskDetails["deadline"] as DateTime,
       );
-      setState(() => _tasks.add(newTask));
     }
   }
 }
---
+++ lib/ui/viewmodel/task_view_model.dart
@@ -0,0 +1,53 @@
+// viewmodels/task_viewmodel.dart
+import 'package:flutter/foundation.dart';
+
+import '../../domain/repository/task_repository.dart';
+import '../../domain/status.dart';
+import '../../domain/task.dart';
+
+class TaskViewModel extends ChangeNotifier {
+  final TaskRepository _taskRepository;
+
+  TaskViewModel(this._taskRepository);
+
+  List<Task> _tasks = [];
+
+  List<Task> get tasks => _tasks;
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  Future<void> loadTasks() async {
+    _isLoading = true;
+    notifyListeners();
+    try {
+      _tasks = await _taskRepository.getTasks();
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error loading tasks: $e');
+      }
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> createTask(String title, String memo, DateTime deadline) async {
+    final newTask = Task(
+      title: title,
+      memo: memo,
+      deadline: deadline,
+      status: Status.incomplete,
+    );
+    try {
+      await _taskRepository.addTask(newTask);
+      await loadTasks();
+    } catch (e) {
+      // エラーハンドリング
+      if (kDebugMode) {
+        print('Error creating task: $e');
+      }
+    }
+  }
+}
